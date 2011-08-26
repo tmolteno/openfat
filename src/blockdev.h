b94c7f6a3489e65c5964959a7c3bbb4165bdf9bf
Gareth McMullin <gareth@blacksphere.co.nz>
2011-08-26 20:08:45 +1200

    Added abstract block device interface definition with sample implementation.

---
+++ src/blockdev.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the openfat project.
+ *
+ * Copyright (C) 2011  Department of Physics, University of Otago
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Block device abstraction.  
+ * Must be implemented by application for a physical device.
+ */
+
+#ifndef __BLOCKDEV_H
+#define __BLOCKDEV_H
+
+#include <stdint.h>
+
+struct block_device {
+	/* Info about the device */
+	uint16_t (*get_sector_size)(struct block_device *dev);
+	/* ... more to be added as needed ... */
+
+	/* Actions on the device */
+	int (*read_sectors)(struct block_device *dev, 
+			uint32_t sector, uint32_t count, void *buf);
+	/* ... more to be added as needed ... */
+
+	/* May be private fields here ... */
+};
+
+/* Convenient wrapper functions */
+static inline uint16_t
+block_get_sector_size(struct block_device *dev)
+{
+	return dev->get_sector_size(dev);
+}
+
+static inline int 
+block_read_sectors(struct block_device *dev,
+		uint32_t sector, uint32_t count, void *buf)
+{
+	return dev->read_sectors(dev, sector, count, buf);
+}
+
+#endif
+
---
+++ src/blockdev_file.c
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the openfat project.
+ *
+ * Copyright (C) 2011  Department of Physics, University of Otago
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Sample block device implementation:
+ * Implementation of abstract block device over a Unix file
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "blockdev.h"
+
+#define FILE_SECTOR_SIZE 512
+
+struct block_device_file {
+	struct block_device bldev;
+	FILE *file;
+};
+
+uint16_t file_get_sector_size(struct block_device *dev)
+{
+	return FILE_SECTOR_SIZE;
+}
+
+int file_read_sectors(struct block_device *bldev,
+		uint32_t sector, uint32_t count, void *buf)
+{
+	struct block_device_file *dev = (void*)bldev;
+ 
+	fseek(dev->file, sector * FILE_SECTOR_SIZE, SEEK_SET);
+	return fread(buf, FILE_SECTOR_SIZE, count, dev->file);
+}
+
+struct block_device * block_device_file_new(const char *filename, const char *mode)
+{
+	FILE *f;
+	struct block_device_file *dev;
+	struct block_device *bldev;
+
+	f = fopen(filename, mode);
+	if(f == NULL)
+		return NULL; 
+
+	dev = malloc(sizeof(*dev));
+	bldev = (void*)dev;
+
+	bldev->read_sectors = file_read_sectors;
+	bldev->get_sector_size = file_get_sector_size;
+	dev->file = f;
+
+	return (struct block_device*)dev;
+}
+
