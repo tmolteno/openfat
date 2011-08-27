b9fead6ff5e0d9566ec4eaf0df84668f678e1057
Gareth McMullin <gareth@blacksphere.co.nz>
2011-08-27 22:24:35 +1200

    Added definition for directory entry (short name only).

---
+++ src/direntry.h
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the openfat project.
+ *
+ * Copyright (C) 2011  Department of Physics, University of Otago
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* FAT Directory entry definitions.
+ */
+
+#define FAT_ATTR_READ_ONLY	0x01
+#define FAT_ATTR_HIDDEN		0x02
+#define FAT_ATTR_SYSTEM		0x04
+#define FAT_ATTR_VOLUME_ID	0x08
+#define FAT_ATTR_DIRECTORY	0x10
+#define FAT_ATTR_ARCHIVE	0x20
+#define FAT_ATTR_LONG_NAME	0x0F
+
+struct fat_dirent {
+	char name[11]; /* DOS file name 8.3 */
+	uint8_t attr;
+	uint8_t Reserved;
+	uint8_t create_time_fine;
+	uint16_t create_time;
+	uint16_t create_date;
+	uint16_t access_date;
+	uint16_t cluster_hi;
+	uint16_t write_time;
+	uint16_t write_date;
+	uint16_t cluster_lo;
+	uint32_t size;
+} __attribute__((packed));
+
